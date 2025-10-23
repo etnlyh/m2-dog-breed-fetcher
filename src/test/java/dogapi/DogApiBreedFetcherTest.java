cb62821d632792352d5be2e99ce228e1da9f6c84
ethan <ethxnoo9@gmail.com>
2025-10-23 06:05:54 -0400

    uncommented out test

--- src/test/java/dogapi/DogApiBreedFetcherTest.java
+++ src/test/java/dogapi/DogApiBreedFetcherTest.java
@@ -11,13 +11,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DogApiBreedFetcherTest {
 
-//    @Test
-//    void testValidBreedReturnsSubBreeds() throws IOException, BreedFetcher.BreedNotFoundException {
-//        BreedFetcher fetcher = new DogApiBreedFetcher();
-//        List<String> subBreeds = fetcher.getSubBreeds("hound");
-//        Set<String> expected = new HashSet<>(List.of("afghan", "basset", "blood", "english", "ibizan", "plott", "walker"));
-//        assertEquals(expected, new HashSet<>(subBreeds));
-//    }
+    @Test
+    void testValidBreedReturnsSubBreeds() throws IOException, BreedFetcher.BreedNotFoundException {
+        BreedFetcher fetcher = new DogApiBreedFetcher();
+        List<String> subBreeds = fetcher.getSubBreeds("hound");
+        Set<String> expected = new HashSet<>(List.of("afghan", "basset", "blood", "english", "ibizan", "plott", "walker"));
+        assertEquals(expected, new HashSet<>(subBreeds));
+    }
 
     @Test
     void testInvalidBreedThrowsException() {
